ffdd4a0b9a0176152c346df5dce353ad085691a5
Nikita <nikita91797@gmail.com>
2024-11-25 16:37:07 +0800

    Lesson 1

---
+++ ur_1/script1.js
@@ -0,0 +1,4 @@
+//задание 1
+let degrees = prompt("Введите градусы цельсия")
+alert("температура в градусах Фаренгейта " + (9 / 5 * parseInt(degrees) + 32))
+console.log((9 / 5 * parseInt(degrees) + 32))
---
+++ ur_1/script2.js
@@ -0,0 +1,5 @@
+//задание 1
+let name = "Nikita"
+let admin = name
+alert(admin)
+console.log(admin)
