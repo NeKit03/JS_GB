80b29047384e0eae64afd58b79b162c30a1e820e
Nikita <nikita91797@gmail.com>
2024-11-27 23:16:27 +0800

    Lesson 5

---
+++ ur_5/script1.js
@@ -0,0 +1,16 @@
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+};
+
+console.log("1 задание");
+for (let key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
